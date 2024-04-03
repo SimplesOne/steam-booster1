cc21cced8af61c763940eacc0c1e76c730a2267f
SimplesOne <db331171@gmail.com>
2024-04-03 17:57:48 +0300

    dawd

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
+const http = require('http');
+const { spawn } = require('child_process');
+
+let childProcess;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/start') {
+        if (!childProcess) {
+            console.log('Starting child process...');
+            childProcess = spawn('node', ['your_existing_script.js']);
+
+            childProcess.stdout.on('data', (data) => {
+                console.log(`Child process stdout: ${data}`);
+            });
+
+            childProcess.stderr.on('data', (data) => {
+                console.error(`Child process stderr: ${data}`);
+            });
+
+            childProcess.on('close', (code) => {
+                console.log(`Child process exited with code ${code}`);
+                childProcess = null;
+            });
+
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Child process started.');
+        } else {
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Child process is already running.');
+        }
+    } else if (req.url === '/stop') {
+        if (childProcess) {
+            console.log('Stopping child process...');
+            childProcess.kill();
+            childProcess = null;
+
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Child process stopped.');
+        } else {
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Child process is not running.');
+        }
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not found.');
+    }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+
+
+
 const fs = require("file-system");
 const path = require("path").join(__dirname, "users");
 const steamClient = require('./client.js');
