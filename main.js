b7b035df6012513491968a66071da319f59c00c1
SimplesOne <db331171@gmail.com>
2024-04-03 18:09:17 +0300

    fixed port

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
